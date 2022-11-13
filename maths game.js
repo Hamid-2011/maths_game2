8060dd4c372cd886645e18ffb4e5c5733df498b0
Hamid-2011 <108743441+Hamid-2011@users.noreply.github.com>
2022-11-13 13:25:12 +0530

    Add files via upload

---
+++ maths game.js
@@ -0,0 +1,24 @@
+function addUser() {
+    player1_name = document.getElementById( "player1_name_input").value;
+    player2_name = document. getElementById("player2_name_input").value;
+
+    localStorage.setItem("player1_name", player1_name);
+    localStorage.setItem("player2_name", player2_name); 
+
+    window. location ="game_page.html";
+
+}
+//set "player1_name" using localstorage.setItem() function
+//set "player2_name" using localstorage.setItem() function
+
+//Get "player1_name" using localstorage.setItem() function and assign it to player1_name
+//Get "player1_name" using localstorage.setItem() function and assign it to player1_name
+
+// create function addUser()
+function ___ {
+    // Get value of uesr by id player1_name_input and  player2_name_input
+
+    // store these values locally
+
+    //Assign "game_page.html" to window.locaction
+}
